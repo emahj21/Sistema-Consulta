1968a3b53b636543bdf49dcea67d1ff113846ffb
RaulWalls <60161477+RaulWalls@users.noreply.github.com>
2022-02-02 10:52:34 -0600

    consulta v1.1

---
+++ conexion.php
@@ -0,0 +1,11 @@
+<?php
+    $conexion = new mysqli("localhost", "root", "", "bd_uni");
+
+    if($conexion)
+    {
+        echo 'Hola prros xD';
+
+    }else{
+        echo 'Falló la conexión';
+    }
+?>
